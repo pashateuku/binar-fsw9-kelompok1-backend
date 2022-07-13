c62c8bc7a541a731c1154148c42a4a0754435cc9
dimas agung <dimasraden0798@gmail.com>
2022-07-13 16:23:02 +0800

    [Dimas] Navbar edit

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -7,7 +7,7 @@ import '../Assets/css/Navbar.css';
 function Navbar() {
   return (
     <>
-      <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
+      <nav className='navbar navbar-expand-lg navbar-dark fixed-top'>
         <div className='container-fluid'>
           {/* logo navbar */}
           <a className='navbar-brand' href='/'>
