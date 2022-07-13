6e1416cd6ced78cdf9a24951f4d40fb467ef62d4
dimas agung <dimasraden0798@gmail.com>
2022-07-13 22:43:15 +0800

    [Dimas] Memindahkan file landingpage kedalam pages

--- client/src/Pages/LandingPage.js
+++ client/src/Pages/LandingPage.js
@@ -1,20 +1,20 @@
 import React from "react";
 //IMPORT GLOBAL STYLING
-import './App.css';
+import '../Assets/css/LandingPage.css';
 //IMPORT REACT ROUTER DOM
 import { BrowserRouter } from "react-router-dom";
 //IMPORT COMPONENTS
-import Navbar from "./Components/Navbar";
-import Footer from "./Components/Footer";
-import HeroLandingPage from "./Components/HeroLandingPage";
+import Navbar from "../Components/Navbar";
+import Footer from "../Components/Footer";
+import HeroLandingPage from "../Components/HeroLandingPage";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Navbar/>
-        <HeroLandingPage/>
-        <Footer/>
+        <Navbar />
+        <HeroLandingPage />
+        <Footer />
       </BrowserRouter>
     </>
   );
--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import LandingPage from "./Pages/LandingPage";
 //IMPORT BOOTSTRAP
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <LandingPage />
   </React.StrictMode>
 );
