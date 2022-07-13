38fb93f3bfb691efdcb236cdbb3bff31e2f06eae
dimas agung <dimasraden0798@gmail.com>
2022-07-13 16:45:11 +0800

    [Dimas] Edit navbar and use column flex

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -9,41 +9,52 @@ function Navbar() {
     <>
       <nav className='navbar navbar-expand-lg navbar-dark fixed-top'>
         <div className='container-fluid'>
-          {/* logo navbar */}
-          <a className='navbar-brand' href='/'>
-            <img className='logo' src={NavbarLogo} alt="logo"/>
-            <h4 className='navbar-text'>TEAM<span> ONE</span></h4>
-          </a>
-          {/* hamburger button */}
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+            <div className='col-4'>
+              {/* logo navbar */}
+              <a className='navbar-brand' href='/'>
+                <img className='logo' src={NavbarLogo} alt="logo"/>
+                <h4 className='navbar-text'>TEAM<span> ONE</span></h4>
+              </a>
+              {/* hamburger button */}
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                   data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                   aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-          </button>
-          {/* navbar list */}
-          <div className='collapse navbar-collapse list-text' id='navbarSupportedContent'>
-            <ul className="navbar-nav m-auto mb-lg-0">
-              <li className="nav-item mx-3">
-                <a className="nav-link active" aria-current="page" href="/">HOME</a>
-              </li>
-              <li className="nav-item mx-3">
-                <a className="nav-link" href="/">LIST GAME</a>
-              </li>
-              <li className="nav-item mx-3">
-                <a className="nav-link" href="/">SUPPORT</a>
-              </li>
-            </ul>
+                  <span class="navbar-toggler-icon"></span>
+              </button>
+            </div>
+            <div className='col-4'>
+              {/* navbar list */}
+              <div className='collapse navbar-collapse list-text' id='navbarSupportedContent'>
+                <ul className="navbar-nav m-auto mb-lg-0">
+                  <li className="nav-item mx-3">
+                    <a className="nav-link active" aria-current="page" href="/">HOME</a>
+                  </li>
+                  <li className="nav-item mx-3">
+                    <a className="nav-link" href="/">LIST GAME</a>
+                  </li>
+                  <li className="nav-item mx-3">
+                    <a className="nav-link" href="/">SUPPORT</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
             {/* button login and register */}
-            <ul class="navbar-nav">
-              <li class="nav-item mx-3">
-                <a class="nav-link" href="/">REGISTER</a>
-              </li>
-              <li class="nav-item mx-3">
-                <a class="nav-link" href="/">LOGIN</a>
-              </li>
-            </ul>
+            <div className='col-4'>
+              <ul class="navbar-nav justify-content-end">
+                <li class="nav-item mx-3">
+                  <a class="nav-link" href="/">REGISTER</a>
+                </li>
+                <li class="nav-item mx-3">
+                  <a class="nav-link" href="/">LOGIN</a>
+                </li>
+              </ul>
+            </div>
+            
+          
+          
+            
+
           </div>
-        </div>
       </nav>
     </>
   )
