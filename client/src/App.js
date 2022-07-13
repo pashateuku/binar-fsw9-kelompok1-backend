94ad30134b40400a173e426f65b3170a69514ba8
dimas agung <dimasraden0798@gmail.com>
2022-07-13 16:21:49 +0800

    [Dimas] Kustomisasi Body

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,19 @@
 import React from "react";
+//IMPORT GLOBAL STYLING
 import './App.css';
+//IMPORT REACT ROUTER DOM
 import { BrowserRouter } from "react-router-dom";
+//IMPORT COMPONENTS
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
+import HeroLandingPage from "./Components/HeroLandingPage";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Navbar/>
+        <HeroLandingPage/>
         <Footer/>
       </BrowserRouter>
     </>
