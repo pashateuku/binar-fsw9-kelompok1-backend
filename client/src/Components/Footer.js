fe5e759bc850a522e90b63ac0f906da56b22c2fa
dimas agung <dimasraden0798@gmail.com>
2022-07-13 15:20:58 +0800

    [Dimas] create simple foter components

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -1,8 +1,24 @@
 import React from 'react';
+//IMPORT LOGO IMAGES
+import NavbarLogo from '../Assets/images/logoteamone.png';
+//IMPORT GLOBAL STYLING
+import '../Assets/css/Footer.css';
+
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <>
+      <footer>
+        <div className='footer-content'>
+          <h4>TEAM<span> ONE</span></h4>
+          <p>All designs and source code are the result of group collaboration team one</p>
+          <img className='logo' src={NavbarLogo} alt="logo"/>
+        </div>
+        <div className='footer-bottom'>
+          <p>Copyright &copy;2022 Team <span>One</span></p>
+        </div>
+      </footer>
+    </>
   )
 }
 
