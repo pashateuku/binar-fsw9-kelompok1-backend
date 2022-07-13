0a6b4beb0c5427e539abc29a733e0a78b9f7a495
dimas agung <dimasraden0798@gmail.com>
2022-07-13 15:21:11 +0800

    [Dimas] Add footer

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import './App.css';
 import { BrowserRouter } from "react-router-dom";
 import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Navbar/>
+        <Footer/>
       </BrowserRouter>
     </>
   );
