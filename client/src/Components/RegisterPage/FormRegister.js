58fad62046e6cec7efa4c4bbfa51cd1859c90ed6
dimas agung <dimasraden0798@gmail.com>
2022-07-15 21:55:42 +0800

    [Dimas] Add function Register

--- client/src/Components/RegisterPage/FormRegister.js
+++ client/src/Components/RegisterPage/FormRegister.js
@@ -1,30 +1,53 @@
-import React from 'react';
+//IMPORT AXIOS
+import axios from 'axios';
+import React, { useState } from 'react';
 //IMPORT GLOBAL STYLING
 import '../../Assets/css/RegisterForm.css';
 
 function FormRegister() {
+    //USESTATE FOR USERNAME AND PASSWORD
+    const [email, setEmail] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    //HANDLE ON SUBMIT
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const result = await axios.post('http://localhost:3000/register', {
+                email: email,
+                username: username,
+                password: password
+            });
+            alert(result.data.message);
+            localStorage.setItem('isAuthenticated', true);
+        }
+        catch (err) {
+            alert(err.toString());
+        }
+    }
     return (
         <>
             <div className='form-content-right'>
-                <form action='/register' method='post' className='form'>
+                <form action='/register' method='post' className='form' onSubmit={handleSubmit}>
                     <h1>Create your account by filling out the information below.</h1>
                     <div className='form-inputs'>
                         <label htmlFor='email' className='form-label'>
                             Email
                         </label>
-                        <input id='email' type="email" name="email" className='form-input' placeholder='Enter your email' />
+                        <input id='email' type="email" name="email" className='form-input' placeholder='Enter your email' onChange={(e) => setEmail(e.target.value)} />
                     </div>
                     <div className='form-inputs'>
                         <label htmlFor='username' className='form-label'>
                             Username
                         </label>
-                        <input id='username' type="text" name="username" className='form-input' placeholder='Enter your username' />
+                        <input id='username' type="text" name="username" className='form-input' placeholder='Enter your username' onChange={(e) => setUsername(e.target.value)} />
                     </div>
                     <div className='form-inputs'>
                         <label htmlFor='password' className='form-label'>
                             Password
                         </label>
-                        <input id='password' type="password" name="password" className='form-input' placeholder='Enter your password' />
+                        <input id='password' type="password" name="password" className='form-input' placeholder='Enter your password' onChange={(e) => setPassword(e.target.value)} />
                     </div>
                     <button className='form-input-btn' type='submit'>Submit</button>
                     <p className='form-input-login'>Already have an account? Login <a href='/login'>here</a></p>
