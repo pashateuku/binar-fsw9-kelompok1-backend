0802342cd07cc87cafea86f696c3da8fa9f0c117
ammrshmbng <ammarmare01@gmail.com>
2022-07-16 20:27:01 +0700

    membuat api register

--- controllers/register.js
+++ controllers/register.js
@@ -1,36 +1,39 @@
-const {user} = require('../models');
+const db = require('../models');
 
-// const register = async (req,res)=>{
+const register = async (req,res)=>{
 
-//     try {
-//         const data = await db.user.create({
-//             username: req.body.username,
-//             email: req.body.email,
-//             password: req.body.password
-//         });
+    try {
+        const data = await db.user.create({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password
+        });
 
-//         return res.status(200).json("anda berhasil register");
+        return res.status(200)
+            .json({
+                message: "anda berhasil register"
+            });
 
-//     } catch (error) {
+    } catch (error) {
         
-//     }
+    }
    
 
     
-// }
-
-function register(req,res){
-
-    user.create({
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password
-    })
-    .then(()=>{
-        res.status(200).json({message:"kamu berhasil login"});
-    });
-
-
 }
+
+// function register(req,res){
+
+//     db.user.create({
+//         username: req.body.username,
+//         email: req.body.email,
+//         password: req.body.password
+//     })
+//     .then(()=>{
+//      return   res.status(200).json({message:"kamu berhasil login"});
+//     });
+
+
+// }
 
 module.exports = {register};
