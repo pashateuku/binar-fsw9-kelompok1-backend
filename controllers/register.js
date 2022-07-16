bf7fbb24ee2ccf89bfd896a879c4d7db1aa8d650
ammrshmbng <ammarmare01@gmail.com>
2022-07-16 19:55:20 +0700

    modify controller/register.js

--- controllers/register.js
+++ controllers/register.js
@@ -1,21 +1,36 @@
-const user = require('../models');
+const {user} = require('../models');
 
-const register = async (req,res)=>{
+// const register = async (req,res)=>{
 
-    try {
-        const data = await user.create({
-            username: req.body.username,
-            email: req.body.email,
-            password: req.body.password
-        });
+//     try {
+//         const data = await db.user.create({
+//             username: req.body.username,
+//             email: req.body.email,
+//             password: req.body.password
+//         });
 
-        return res.status(200).json("anda berhasil login");
-    } catch (error) {
+//         return res.status(200).json("anda berhasil register");
+
+//     } catch (error) {
         
-    }
+//     }
    
 
     
+// }
+
+function register(req,res){
+
+    user.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    })
+    .then(()=>{
+        res.status(200).json({message:"kamu berhasil login"});
+    });
+
+
 }
 
-module.exports = register;
+module.exports = {register};
--- router/index.js
+++ router/index.js
@@ -2,7 +2,9 @@ const router = require("express").Router();
 
 // import controllers
 const home = require('../controllers/home');
+const {register} = require('../controllers/register');
 
-router.use("/", home.index);
+router.get("/", home.index);
+router.post("/register",register)
 
 module.exports = router;
