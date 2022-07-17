a98518a578b768ef919ea1a3c2b57c1c61a4da80
ammrshmbng <ammarmare01@gmail.com>
2022-07-17 21:37:04 +0700

    membuat hash untuk password

--- controllers/register.js
+++ controllers/register.js
@@ -1,12 +1,18 @@
 const {user} = require('../models');
+const bcrypt = require('bcrypt');
+
+const saltRound = Number(process.env.APP_HASH_ROUND) || 10;
 
 const register = async (req,res)=>{
 
     try {
+            // buat sandi encrypted dengan bcrypt
+        const encryptedPassword = await bcrypt.hash(req.body.password, saltRound);
+
         const data = user.create({
             username: req.body.username,
             email: req.body.email,
-            password: req.body.password
+            password: encryptedPassword
         });
 
         return res.status(200)
