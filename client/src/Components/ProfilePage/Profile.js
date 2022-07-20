1a2c6932a1c4c77fcc7073a92a57172268fe70b7
dimas agung <dimasraden0798@gmail.com>
2022-07-20 13:13:24 +0800

    [Dimas] add profile page

--- client/src/Components/ProfilePage/Profile.js
+++ client/src/Components/ProfilePage/Profile.js
@@ -1,19 +1,62 @@
 import React from 'react'
+import { Button } from 'reactstrap'
+import ProfileAchievement from './ProfileAchievement';
 import PP from '../../Assets/images/pp.jpg';
 import '../../Assets/css/Profile.css'
 
 function Profile() {
     return (
         <>
-            <div className='container-fluid d-flex bg-light min-vh-100'>
-                <div className='row bg-light'>
-                    <div className='col-5 pt-5 px-5'>
-                        <div className='card'>
-                            <img className='profile-pict' src={PP} alt="profile" />
+            <div className='container-fluid bg-light min-vh-100'>
+                <div className='row bg-light '>
+                    <div className='col-3 pt-5 px-5'>
+                        <div className='card pp-section bg-light'>
+                            <img className='profile-pict mb-3' src={PP} alt="profile" />
+                            <Button className='btn-change-profile' color='danger'>Change Profile Picture</Button>
                         </div>
                     </div>
-                    <div className='col-7 pt-5 px-5'>
-                        <h1>Halaman Data Informasi</h1>
+                    <div className='col-9 pt-5 px-5 bg-white'>
+                        <div className='container'>
+                            <div className='row'>
+                                <div className='col'>
+                                    <h1>USER PROFILE</h1>
+                                </div>
+                                <div className='col text-end'>
+                                    <Button color='danger' outline>Edit profile</Button>
+                                </div>
+                            </div>
+                            <table class="table table-bordered">
+                                <tbody>
+                                    <tr>
+                                        <td>Name</td>
+                                        <td>Dimas Agung</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Username</td>
+                                        <td>Dimasagungw</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Email</td>
+                                        <td>Dimas@gmail.com</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Password</td>
+                                        <td>*********</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Social Media URL</td>
+                                        <td><a href='https://www.instagram.com/'>www.instagram.com/akudimas</a></td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            <div class="mb-3">
+                                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3">Halo nama saya dimas saya senang berteman dengan siapa saja, ayo bermain bersama dan have fun didalam game</textarea>
+                            </div>
+                            <div className='row'>
+                                <ProfileAchievement />
+
+                            </div>
+                        </div>
                     </div>
                 </div>
 
---
+++ client/src/Components/ProfilePage/ProfileAchievement.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Button } from 'reactstrap';
+import { NavLink } from 'react-router-dom';
+//IMPORT ICON
+import { FaBattleNet, FaNapster, FaSkyatlas, FaTeamspeak } from "react-icons/fa";
+// IMPORT STYLING
+import '../../Assets/css/Profile.css'
+
+function ProfileAchievement() {
+    return (
+        <>
+            <div class="row px-5 py-5 bg-white">
+                <div className='pb-3'>
+                    <h2 className='text-center fw-bold'>USER ACHIEVEMENT</h2>
+                </div>
+                <div class="col-sm-3">
+                    <div class="card card-size-profile">
+                        <div class="card-body card-custom">
+                            <FaBattleNet className='card-icon ms-3' id='icon-achievement' />
+                            <h5 class="card-title">Right Back At Ya</h5>
+                            <p class="card-text">Kill or incapacitate an enemy who threw a grenade at you.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="card card-size-profile">
+                        <div class="card-body card-custom">
+                            <FaNapster className='card-icon ms-3' id='icon-achievement' />
+                            <h5 class="card-title">True Soldier</h5>
+                            <p class="card-text">Kill or incapacitate 300 enemies using ranged weapons.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="card card-size-profile">
+                        <div class="card-body card-custom">
+                            <FaSkyatlas className='card-icon ms-3' id='icon-achievement' />
+                            <h5 class="card-title">Stanislavski's Method</h5>
+                            <p class="card-text">Use a dialogue option related to V's life path 10 times.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="card card-size-profile">
+                        <div class="card-body card-custom">
+                            <FaTeamspeak className='card-icon ms-3' id='icon-achievement' />
+                            <h5 class="card-title">Two Heads, One Bullet</h5>
+                            <p class="card-text">Kill or incapacitate 2 enemies with the same sniper rifle shot.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className='row bg-light'>
+                <div className='text-center mt-2 mb-5'>
+                    <NavLink to='./list-game'><Button className='btn-list btn-all-game'>SEE ALL GAME ACHIEVEMENT</Button></NavLink>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default ProfileAchievement
